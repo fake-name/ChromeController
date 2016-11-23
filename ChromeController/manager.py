bc1af4c9ade9ff074cdd407c4a422e00f56206af
Fake-Name <something@fake-url.com>
2016-11-22 21:35:44 -0800

    Minor bits and bobs. Mostly cleanup in the manager subclass.

--- ChromeController/manager.py
+++ ChromeController/manager.py
@@ -17,295 +17,6 @@ CromeRemoteDebugInterfaceBase = gen.get_class_def()
 
 from .resources import js
 
-# [
-# 	'Accessibility_getPartialAXTree',
-# 	'Animation_disable',
-# 	'Animation_enable',
-# 	'Animation_getCurrentTime',
-# 	'Animation_getPlaybackRate',
-# 	'Animation_releaseAnimations',
-# 	'Animation_resolveAnimation',
-# 	'Animation_seekAnimations',
-# 	'Animation_setPaused',
-# 	'Animation_setPlaybackRate',
-# 	'Animation_setTiming',
-# 	'ApplicationCache_enable',
-# 	'ApplicationCache_getApplicationCacheForFrame',
-# 	'ApplicationCache_getFramesWithManifests',
-# 	'ApplicationCache_getManifestForFrame',
-# 	'CSS_addRule',
-# 	'CSS_collectClassNames',
-# 	'CSS_createStyleSheet',
-# 	'CSS_disable',
-# 	'CSS_enable',
-# 	'CSS_forcePseudoState',
-# 	'CSS_getBackgroundColors',
-# 	'CSS_getComputedStyleForNode',
-# 	'CSS_getInlineStylesForNode',
-# 	'CSS_getLayoutTreeAndStyles',
-# 	'CSS_getMatchedStylesForNode',
-# 	'CSS_getMediaQueries',
-# 	'CSS_getPlatformFontsForNode',
-# 	'CSS_getStyleSheetText',
-# 	'CSS_setEffectivePropertyValueForNode',
-# 	'CSS_setKeyframeKey',
-# 	'CSS_setMediaText',
-# 	'CSS_setRuleSelector',
-# 	'CSS_setStyleSheetText',
-# 	'CSS_setStyleTexts',
-# 	'CSS_startRuleUsageTracking',
-# 	'CSS_stopRuleUsageTracking',
-# 	'CacheStorage_deleteCache',
-# 	'CacheStorage_deleteEntry',
-# 	'CacheStorage_requestCacheNames',
-# 	'CacheStorage_requestEntries',
-# 	'DOMDebugger_getEventListeners',
-# 	'DOMDebugger_removeDOMBreakpoint',
-# 	'DOMDebugger_removeEventListenerBreakpoint',
-# 	'DOMDebugger_removeInstrumentationBreakpoint',
-# 	'DOMDebugger_removeXHRBreakpoint',
-# 	'DOMDebugger_setDOMBreakpoint',
-# 	'DOMDebugger_setEventListenerBreakpoint',
-# 	'DOMDebugger_setInstrumentationBreakpoint',
-# 	'DOMDebugger_setXHRBreakpoint',
-# 	'DOMStorage_disable',
-# 	'DOMStorage_enable',
-# 	'DOMStorage_getDOMStorageItems',
-# 	'DOMStorage_removeDOMStorageItem',
-# 	'DOMStorage_setDOMStorageItem',
-# 	'DOM_collectClassNamesFromSubtree',
-# 	'DOM_copyTo',
-# 	'DOM_disable',
-# 	'DOM_discardSearchResults',
-# 	'DOM_enable',
-# 	'DOM_focus',
-# 	'DOM_getAttributes',
-# 	'DOM_getBoxModel',
-# 	'DOM_getDocument',
-# 	'DOM_getHighlightObjectForTest',
-# 	'DOM_getNodeForLocation',
-# 	'DOM_getOuterHTML',
-# 	'DOM_getRelayoutBoundary',
-# 	'DOM_getSearchResults',
-# 	'DOM_hideHighlight',
-# 	'DOM_highlightFrame',
-# 	'DOM_highlightNode',
-# 	'DOM_highlightQuad',
-# 	'DOM_highlightRect',
-# 	'DOM_markUndoableState',
-# 	'DOM_moveTo',
-# 	'DOM_performSearch',
-# 	'DOM_pushNodeByPathToFrontend',
-# 	'DOM_pushNodesByBackendIdsToFrontend',
-# 	'DOM_querySelector',
-# 	'DOM_querySelectorAll',
-# 	'DOM_redo',
-# 	'DOM_removeAttribute',
-# 	'DOM_removeNode',
-# 	'DOM_requestChildNodes',
-# 	'DOM_requestNode',
-# 	'DOM_resolveNode',
-# 	'DOM_setAttributeValue',
-# 	'DOM_setAttributesAsText',
-# 	'DOM_setFileInputFiles',
-# 	'DOM_setInspectMode',
-# 	'DOM_setInspectedNode',
-# 	'DOM_setNodeName',
-# 	'DOM_setNodeValue',
-# 	'DOM_setOuterHTML',
-# 	'DOM_undo',
-# 	'Database_disable',
-# 	'Database_enable',
-# 	'Database_executeSQL',
-# 	'Database_getDatabaseTableNames',
-# 	'DeviceOrientation_clearDeviceOrientationOverride',
-# 	'DeviceOrientation_setDeviceOrientationOverride',
-# 	'Emulation_canEmulate',
-# 	'Emulation_clearDeviceMetricsOverride',
-# 	'Emulation_clearGeolocationOverride',
-# 	'Emulation_forceViewport',
-# 	'Emulation_resetPageScaleFactor',
-# 	'Emulation_resetViewport',
-# 	'Emulation_setCPUThrottlingRate',
-# 	'Emulation_setDeviceMetricsOverride',
-# 	'Emulation_setEmulatedMedia',
-# 	'Emulation_setGeolocationOverride',
-# 	'Emulation_setPageScaleFactor',
-# 	'Emulation_setScriptExecutionDisabled',
-# 	'Emulation_setTouchEmulationEnabled',
-# 	'Emulation_setVirtualTimePolicy',
-# 	'Emulation_setVisibleSize',
-# 	'IO_close',
-# 	'IO_read',
-# 	'IndexedDB_clearObjectStore',
-# 	'IndexedDB_disable',
-# 	'IndexedDB_enable',
-# 	'IndexedDB_requestData',
-# 	'IndexedDB_requestDatabase',
-# 	'IndexedDB_requestDatabaseNames',
-# 	'Input_dispatchKeyEvent',
-# 	'Input_dispatchMouseEvent',
-# 	'Input_dispatchTouchEvent',
-# 	'Input_emulateTouchFromMouseEvent',
-# 	'Input_synthesizePinchGesture',
-# 	'Input_synthesizeScrollGesture',
-# 	'Input_synthesizeTapGesture',
-# 	'Inspector_disable',
-# 	'Inspector_enable',
-# 	'LayerTree_compositingReasons',
-# 	'LayerTree_disable',
-# 	'LayerTree_enable',
-# 	'LayerTree_loadSnapshot',
-# 	'LayerTree_makeSnapshot',
-# 	'LayerTree_profileSnapshot',
-# 	'LayerTree_releaseSnapshot',
-# 	'LayerTree_replaySnapshot',
-# 	'LayerTree_snapshotCommandLog',
-# 	'Log_clear',
-# 	'Log_disable',
-# 	'Log_enable',
-# 	'Log_startViolationsReport',
-# 	'Log_stopViolationsReport',
-# 	'Memory_getDOMCounters',
-# 	'Memory_setPressureNotificationsSuppressed',
-# 	'Memory_simulatePressureNotification',
-# 	'Network_addBlockedURL',
-# 	'Network_canClearBrowserCache',
-# 	'Network_canClearBrowserCookies',
-# 	'Network_canEmulateNetworkConditions',
-# 	'Network_clearBrowserCache',
-# 	'Network_clearBrowserCookies',
-# 	'Network_deleteCookie',
-# 	'Network_disable',
-# 	'Network_emulateNetworkConditions',
-# 	'Network_enable',
-# 	'Network_getCertificate',
-# 	'Network_getCookies',
-# 	'Network_getResponseBody',
-# 	'Network_removeBlockedURL',
-# 	'Network_replayXHR',
-# 	'Network_setBypassServiceWorker',
-# 	'Network_setCacheDisabled',
-# 	'Network_setCookie',
-# 	'Network_setDataSizeLimitsForTest',
-# 	'Network_setExtraHTTPHeaders',
-# 	'Network_setMonitoringXHREnabled',
-# 	'Network_setUserAgentOverride',
-# 	'Page_addScriptToEvaluateOnLoad',
-# 	'Page_captureScreenshot',
-# 	'Page_clearDeviceMetricsOverride',
-# 	'Page_clearDeviceOrientationOverride',
-# 	'Page_clearGeolocationOverride',
-# 	'Page_configureOverlay',
-# 	'Page_deleteCookie',
-# 	'Page_disable',
-# 	'Page_enable',
-# 	'Page_getAppManifest',
-# 	'Page_getCookies',
-# 	'Page_getLayoutMetrics',
-# 	'Page_getNavigationHistory',
-# 	'Page_getResourceContent',
-# 	'Page_getResourceTree',
-# 	'Page_handleJavaScriptDialog',
-# 	'Page_navigate',
-# 	'Page_navigateToHistoryEntry',
-# 	'Page_processNavigation',
-# 	'Page_reload',
-# 	'Page_removeScriptToEvaluateOnLoad',
-# 	'Page_requestAppBanner',
-# 	'Page_screencastFrameAck',
-# 	'Page_searchInResource',
-# 	'Page_setAutoAttachToCreatedPages',
-# 	'Page_setColorPickerEnabled',
-# 	'Page_setControlNavigations',
-# 	'Page_setDeviceMetricsOverride',
-# 	'Page_setDeviceOrientationOverride',
-# 	'Page_setDocumentContent',
-# 	'Page_setGeolocationOverride',
-# 	'Page_setTouchEmulationEnabled',
-# 	'Page_startScreencast',
-# 	'Page_stopScreencast',
-# 	'Rendering_setShowDebugBorders',
-# 	'Rendering_setShowFPSCounter',
-# 	'Rendering_setShowPaintRects',
-# 	'Rendering_setShowScrollBottleneckRects',
-# 	'Rendering_setShowViewportSizeOnResize',
-# 	'Security_disable',
-# 	'Security_enable',
-# 	'Security_showCertificateViewer',
-# 	'ServiceWorker_deliverPushMessage',
-# 	'ServiceWorker_disable',
-# 	'ServiceWorker_dispatchSyncEvent',
-# 	'ServiceWorker_enable',
-# 	'ServiceWorker_inspectWorker',
-# 	'ServiceWorker_setForceUpdateOnPageLoad',
-# 	'ServiceWorker_skipWaiting',
-# 	'ServiceWorker_startWorker',
-# 	'ServiceWorker_stopWorker',
-# 	'ServiceWorker_unregister',
-# 	'ServiceWorker_updateRegistration',
-# 	'Storage_clearDataForOrigin',
-# 	'SystemInfo_getInfo',
-# 	'Target_activateTarget',
-# 	'Target_attachToTarget',
-# 	'Target_closeTarget',
-# 	'Target_createBrowserContext',
-# 	'Target_createTarget',
-# 	'Target_detachFromTarget',
-# 	'Target_disposeBrowserContext',
-# 	'Target_getTargetInfo',
-# 	'Target_getTargets',
-# 	'Target_sendMessageToTarget',
-# 	'Target_setAttachToFrames',
-# 	'Target_setAutoAttach',
-# 	'Target_setDiscoverTargets',
-# 	'Target_setRemoteLocations',
-# 	'Tethering_bind',
-# 	'Tethering_unbind',
-# 	'Tracing_end',
-# 	'Tracing_getCategories',
-# 	'Tracing_recordClockSyncMarker',
-# 	'Tracing_requestMemoryDump',
-# 	'Tracing_start',
-# 	'_ChromeInterface__check_ret',
-# 	'__class__',
-# 	'__del__',
-# 	'__delattr__',
-# 	'__dict__',
-# 	'__dir__',
-# 	'__doc__',
-# 	'__eq__',
-# 	'__format__',
-# 	'__ge__',
-# 	'__getattribute__',
-# 	'__gt__',
-# 	'__hash__',
-# 	'__init__',
-# 	'__le__',
-# 	'__lt__',
-# 	'__module__',
-# 	'__ne__',
-# 	'__new__',
-# 	'__reduce__',
-# 	'__reduce_ex__',
-# 	'__repr__',
-# 	'__setattr__',
-# 	'__sizeof__',
-# 	'__str__',
-# 	'__subclasshook__',
-# 	'__weakref__',
-# 	'blocking_navigate',
-# 	'close',
-# 	'cr_proc',
-# 	'drain_transport',
-# 	'set_headers',
-# 	'set_user_agent_string',
-# 	'set_viewport_size',
-# 	'synchronous_command',
-# 	'take_screeshot',
-# 	'transport'
-# ]
-
 
 class CromeRemoteDebugInterface(CromeRemoteDebugInterfaceBase):
 
@@ -317,67 +28,49 @@ class CromeRemoteDebugInterface(CromeRemoteDebugInterfaceBase):
 		resp3 = self.Network_enable()
 
 
-	def set_viewport_size(self, x_pix, y_pix):
+
+	def update_headers(self, header_args):
 		'''
-		Set the emulated viewport size to `x_pix` by `y_pix`
+		Given a set of headers, update both the user-agent
+		and additional headers for the remote browser.
 
-		'''
-		ret = self.Emulation_setVisibleSize(width = int(x_pix), height = int(y_pix))
+		header_args must be a dict. Keys are the names of
+		the corresponding HTTP header.
 
-		# Command has an empty return
-		return ret
-
-
-	def set_user_agent_string(self, ua_string):
-		'''
-		Override the user-agent string for the remote browser, setting it to `ua_string`.
-		'''
-		# "name": "setUserAgentOverride",
-		# "description": "Allows overriding user agent with the given string.",
-		# "parameters": [
-		#     { "name": "userAgent", "type": "string", "description": "User agent to use." }
-
-		ret = self.Network_setUserAgentOverride(userAgent = ua_string)
-
-		# Command has an empty return
-		return ret
-
-	def set_headers(self, header_dict):
-		'''
-		Update or add a set of headers to the remote browser.
-
-		Passed headers should be a dictionary of key-value items, where the key is the name
-		of the header, and value is the header content.
+		return value is a 2-tuple of the results of the user-agent
+		update, as well as the extra headers update.
+		If no 'User-Agent' key is present in the new headers,
+		the first item in the tuple will be None
 
 		'''
-		# {
-		#     "name": "setExtraHTTPHeaders",
-		#     "description": "Specifies whether to always send extra HTTP headers with the requests from this page.",
-		#     "parameters": [
-		#         { "name": "headers", "$ref": "Headers", "description": "Map with extra HTTP headers." }
-		#     ]
-		# },
 
-		ret = self.Network_setExtraHTTPHeaders(headers = header_dict)
-		# Command has an empty return
+		ua = header_args.pop('User-Agent', None)
+		ret_1 = None
+		if ua:
+			ret_1 = self.Network_setUserAgentOverride(userAgent=ua)
 
-		return ret
 
-	# Interact with httplib.cookie instances
-	def set_cookie(self, cookie):
-		'''
-		TODO
-		'''
-		pass
+		ret_2 = self.Network_setExtraHTTPHeaders(headers = header_args)
 
-	def get_cookie(self):
-		'''
-		TODO
-		'''
-		pass
+		return (ret_1, ret_2)
+
 
 	def __exec_js(self, script, args={}):
 		'''
+
+		Execute the passed javascript statement, optionally with passed
+		arguments.
+
+		Note that if `script` is not a function, it must be a single statement.
+		The presence of semicolons not enclosed in a bracket scope will produce
+		an error.
+
+
+		'''
+
+		'''
+		How chromedriver does this:
+
 		 std::unique_ptr<base::Value>* result) {
 		  std::string json;
 		  base::JSONWriter::Write(args, &json);
@@ -396,16 +89,45 @@ class CromeRemoteDebugInterface(CromeRemoteDebugInterfaceBase):
 
 		resp3 = self.Runtime_evaluate(expression=expression)
 
-		print("Return value: ", resp3)
-
 		return resp3
 
+
+
+
+	# Interact with httplib.cookie instances
+	def set_cookie(self, cookie):
+		'''
+		TODO
+		'''
+		pass
+
+	def get_cookie(self):
+		'''
+		TODO
+		'''
+		pass
+
 	def navigate_to(self, url):
+		'''
+		Trigger a page navigation to url `url`.
+
+		Note that this is done via javascript injection, and as such results in
+		the `referer` header being sent with the url of the network location.
+
+		This is useful when a page's navigation is stateful, or for simple
+		cases of referrer spoofing.
+
+		'''
+
 		assert "'" not in url
 		return self.__exec_js("window.location.href = '{}'".format(url))
 
 
 	def click_link_containing_url(self, url):
+		'''
+		TODO
+
+		'''
 
 		# exec_func =
 
@@ -484,6 +206,10 @@ class CromeRemoteDebugInterface(CromeRemoteDebugInterfaceBase):
 
 	def click_element(self, contains_url):
 		'''
+
+		TODO
+
+
 		ChromeDriver source for how to click an element:
 
 		Status ExecuteClickElement(Session* session,
@@ -531,6 +257,8 @@ class CromeRemoteDebugInterface(CromeRemoteDebugInterfaceBase):
 		}
 		'''
 
+		pass
+
 
 
 
@@ -547,6 +275,10 @@ class CromeRemoteDebugInterface(CromeRemoteDebugInterfaceBase):
 		function will not reflect the changes made by the javascript.
 
 		The rendered page content can be retreived by calling `get_rendered_page_source()`.
+
+		Due to the remote api structure, accessing the raw content after the content has been loaded
+		is not possible, so any task requiring the raw content must be careful to request it
+		before it actually navigates to said content.
 
 		Return value is a dictionary with two keys:
 		{
@@ -579,6 +311,9 @@ class CromeRemoteDebugInterface(CromeRemoteDebugInterfaceBase):
 
 		This is done by looking up the root DOM node, and then requesting the outer HTML
 		for that node ID.
+
+		This calls return will reflect any modifications made by javascript to the
+		page. For unmodified content, use `blocking_navigate_and_get_source()`
 		'''
 
 		dom_attr = self.DOM_getDocument(depth=1, traverseFrames=False)
@@ -625,7 +360,8 @@ class CromeRemoteDebugInterface(CromeRemoteDebugInterfaceBase):
 		 - Page.frameStoppedLoading" (with frameid)
 		 - Page.loadEventFired" (not attached to an ID)
 
-		Therefore, we trigger a navigation option, and then wait for the expected set of results.
+		Therefore, this call triggers a navigation option,
+		and then waits for the expected set of response event messages.
 
 
 		'''
--- test.py
+++ test.py
@@ -103,13 +103,11 @@ def test():
 	cr = ChromeController.CromeRemoteDebugInterface(binary=crbin)
 
 	# print(cr)
-	resp = cr.set_viewport_size(1500, 1000)
+	resp = cr.Emulation_setVisibleSize(1500, 1000)
 	# print("Viewport size", resp)
 
-	resp = cr.set_user_agent_string(ua.pop('User-Agent'))
-	# print("Set user agent: ", resp)
-	ua['X-Devtools-Emulate-Network-Conditions-Client-Id'] = None
-	resp = cr.set_headers(ua)
+	resp = cr.update_headers(ua)
+	print("Set user agent: ", resp)
 	# print("Set extra headers: ", resp)
 
 	# resp = cr.blocking_navigate("http://www.google.com")
