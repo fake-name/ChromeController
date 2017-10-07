3f5bfe389fb337ff23024bdce04574743bd7eef5
Fake-Name <something@fake-url.com>
2017-10-06 22:53:34 -0700

    Documentation!

--- ChromeController/chrome_context.py
+++ ChromeController/chrome_context.py
@@ -3,7 +3,13 @@
 from .manager import ChromeRemoteDebugInterface
 
 class ChromeContext(object):
+	'''
+	Context manager for conveniently handling the lifetime of the underlying chromium instance.
 
+	In general, this should be the preferred way to use an instance of `ChromeRemoteDebugInterface`.
+
+	All parameters are forwarded through to the underlying ChromeRemoteDebugInterface() constructor.
+	'''
 
 	def __init__(self, *args, **kwargs):
 		self.args = args
@@ -15,5 +21,4 @@ class ChromeContext(object):
 		return self.chrome_instance
 
 	def __exit__(self, *args):
-		print("Context manager exit!")
 		self.chrome_instance.close()
--- gen_doc.sh
+++ gen_doc.sh
@@ -31,19 +31,17 @@ python3 -m pydoc -w ChromeController.Generator.gen
 python3 -m pydoc -w ChromeController
 python3 -m pydoc -w ChromeController.manager
 python3 -m pydoc -w ChromeController.cr_exceptions
+python3 -m pydoc -w ChromeController.chrome_context
 python3 -m pydoc -w ChromeController.manager_base
 python3 -m pydoc -w ChromeController.transport
 
-mv ChromeController.ChromeRemoteDebugInterface.html ./docs/ChromeController.ChromeRemoteDebugInterface.html
-mv ChromeController.ChromeSocketManager.html ./docs/ChromeController.ChromeSocketManager.html
-mv ChromeController.Generator.html ./docs/ChromeController.Generator.html
-mv ChromeController.Generator.gen.html ./docs/ChromeController.Generator.gen.html
-mv ChromeController.html ./docs/ChromeController.html
-
-mv ChromeController.manager.html ./docs/ChromeController.manager.html
-mv ChromeController.manager_base.html ./docs/ChromeController.manager_base.html
-mv ChromeController.transport.html ./docs/ChromeController.transport.html
-mv ChromeController.cr_exceptions.html ./docs/ChromeController.cr_exceptions.html
-
-
+mv ChromeController.chrome_context.html                ./docs/ChromeController.chrome_context.html
+mv ChromeController.cr_exceptions.html                 ./docs/ChromeController.cr_exceptions.html
+mv ChromeController.Generator.gen.html                 ./docs/ChromeController.Generator.gen.html
+mv ChromeController.Generator.html                     ./docs/ChromeController.Generator.html
+mv ChromeController.html                               ./docs/ChromeController.html
+mv ChromeController.manager_base.html                  ./docs/ChromeController.manager_base.html
+mv ChromeController.manager.html                       ./docs/ChromeController.manager.html
+mv ChromeController.transport.html                     ./docs/ChromeController.transport.html
+mv ChromeController.ChromeRemoteDebugInterface.html    ./docs/ChromeController.ChromeRemoteDebugInterface.html
 
